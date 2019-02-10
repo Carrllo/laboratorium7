5538daafb76f066423b12cfd636825bcbc0878ec
Carrllo <46500578+Carrllo@users.noreply.github.com>
2019-02-10 10:48:45 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iosream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+string imie;
+cout <<"podaj swoje imie:  ";
+cin >> imie;
+cout <<"witaj w dockerze:" <<imie<<"!  cierpliwosci zycze.\n";
+retur 0;
+}
